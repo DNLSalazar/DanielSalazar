e302ea1c23732ceb9844b3d87919cd75322f8e09
Daniel Salazar <dnlsalazar27@gmail.com>
2019-09-20 23:47:14 -0400

    Updating page

--- scripts/js.js
+++ scripts/js.js
@@ -1,18 +1,25 @@
+
+window.addEventListener("load", function() {
+
+})
+
+
+
 function fullmenu(data) {
-    data.parentElement.classList.toggle("change");
+  data.parentElement.classList.toggle("change");
 }
 
 function focusIn(data) {
-    data.parentElement.lastElementChild.firstChild.style.transform = "translateY(-30px)";
-    data.style.transform = "scale(1,1)";
+  data.parentElement.lastElementChild.firstChild.style.transform = "translateY(-30px)";
+  data.style.transform = "scale(1,1)";
 }
 
 function nofocus(data) {
-    if(data.value != "") {
-        data.parentElement.lastElementChild.firstChild.style.transform = "translateY(-30px)";
-        data.style.transform = "scale(1,1)";
-    } else {
-        data.parentElement.lastElementChild.firstChild.style.transform = "translateY(0px)";
-        data.style.transform = "scale(1,0)";
-    }
+  if(data.value != "") {
+    data.parentElement.lastElementChild.firstChild.style.transform = "translateY(-30px)";
+    data.style.transform = "scale(1,1)";
+  } else {
+    data.parentElement.lastElementChild.firstChild.style.transform = "translateY(0px)";
+    data.style.transform = "scale(1,0)";
+  }
 }
---
+++ scripts/writeAnimation.js
@@ -0,0 +1,39 @@
+var i = 0;
+var arrayWords = ["WEB DEVELOPER", "CODE MANIAC", "self-taught", "GEEK", "GAMER" ,"NBA FAN"];
+var wordElement = document.getElementById("word");
+var currentWord = wordElement.innerHTML;
+var status = 0;
+var timeout;
+const time = 100;
+
+window.addEventListener("load", function () {
+  setTimeout(animation, time);
+});
+
+
+function animation () {
+  timeout = time;
+  if (status == 0) {
+    if (currentWord.length < arrayWords[i].length) {
+      currentWord += arrayWords[i].slice(currentWord.length, currentWord.length+1);
+      wordElement.innerHTML = currentWord;
+    } else {
+      timeout = 1000;
+      status = 1;
+      setTimeout(animation, timeout);
+      return;
+    }
+  } else {
+    if (currentWord.length > 0 && status == 1) {
+      currentWord = currentWord.slice(0, currentWord.length - 1);
+      wordElement.innerHTML = currentWord;
+    } else {
+      i++;
+      if(i == arrayWords.length) i = 0;
+      status = 0;
+      setTimeout(animation, timeout);
+      return;
+    }
+  }
+  setTimeout(animation, timeout);
+}
